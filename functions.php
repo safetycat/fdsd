112f38921da0d66e0f49f93ef53949a2345f6049
elvis <elvina@safetycat.org>
2014-10-31 10:40:21 +0000

    functions: add top nav, include tags in wp search

--- functions.php
+++ functions.php
@@ -263,8 +263,27 @@ add_action('wp_enqueue_scripts ', 'bones_fonts');
 //////// AFTER BONES //////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
 
+// Register top navigation
+register_nav_menus( array(
+  'minitop' => 'Mini Top Navigation Menu'
+) );
 
 
+// Add search box to menu
+function add_search_to_wp_menu ( $items, $args ) {
+  if( 'minitop' === $args -> theme_location ) {
+$items .= '<li class="menu-item menu-item-search">';
+$items .= '<form method="get" class="menu-search-form" action="' . get_bloginfo('home') . '/"><input class="text_input" type="text" value="Search" name="s" id="s" onfocus="if (this.value == \'Search\') {this.value = \'\';}" onblur="if (this.value == \'\') {this.value = \'Search\';}" /><input type="submit" class="my-wp-search" id="searchsubmit" value="search" /></form>';
+$items .= '</li>';
+  }
+return $items;
+}
+add_filter('wp_nav_menu_items','add_search_to_wp_menu',10,2);
+
+
+
+
+// Remove duplicate admin pages for themes & topics
 function tw_remove_menu_pages() {
 
 	remove_submenu_page( 'edit.php?post_type=idea', 'edit-tags.php?taxonomy=themes&amp;post_type=idea' );
@@ -284,6 +303,53 @@ function tw_remove_menu_pages() {
 add_action( 'admin_menu', 'tw_remove_menu_pages', 999 );
 
 
+// Include tags in search
+// add_action( 'pre_get_posts', 'include_tags_in_search' );
+// function include_tags_in_search($query){
+//     if($query->is_search){
+//         $terms = explode(' ', $query->get('s'));
+//         $query->set('tax_query', array(
+//             'relation'=>'OR',
+//             array(
+//                 'taxonomy'=>'post_tag',
+//                 'field'=>'slug',
+//                 'terms'=>$terms
+//             )
+//         ));
+//     }
+// }
 
+
+function custom_search_where($where){
+  global $wpdb;
+  if (is_search())
+    $where .= "OR (t.name LIKE '%".get_search_query()."%' AND {$wpdb->posts}.post_status = 'publish')";
+  return $where;
+}
+
+function custom_search_join($join){
+  global $wpdb;
+  if (is_search())
+    $join .= "LEFT JOIN {$wpdb->term_relationships} tr ON {$wpdb->posts}.ID = tr.object_id INNER JOIN {$wpdb->term_taxonomy} tt ON tt.term_taxonomy_id=tr.term_taxonomy_id INNER JOIN {$wpdb->terms} t ON t.term_id = tt.term_id";
+  return $join;
+}
+
+function custom_search_groupby($groupby){
+  global $wpdb;
+
+  // we need to group on post ID
+  $groupby_id = "{$wpdb->posts}.ID";
+  if(!is_search() || strpos($groupby, $groupby_id) !== false) return $groupby;
+
+  // groupby was empty, use ours
+  if(!strlen(trim($groupby))) return $groupby_id;
+
+  // wasn't empty, append ours
+  return $groupby.", ".$groupby_id;
+}
+
+/*add_filter('posts_where','custom_search_where');
+add_filter('posts_join', 'custom_search_join');
+add_filter('posts_groupby', 'custom_search_groupby');*/
 
 /* DON'T DELETE THIS CLOSING TAG */ ?>
