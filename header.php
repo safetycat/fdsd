bd601de4aca3bb15972490666d326d9f92949d9c
elvis <elvina@safetycat.org>
2014-10-31 10:40:55 +0000

    add mini top nav

--- header.php
+++ header.php
@@ -46,7 +46,23 @@
 			<header class="header" role="banner">
 
 				<div id="inner-header" class="wrap cf">
+					<div id="topbar">
+						<?php wp_nav_menu(array(
+    					'container' => false,                           // remove nav container
+    					'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+    					'menu' => __( 'The Mini Top Menu', 'bonestheme' ),  // nav name
+    					'menu_class' => 'nav mini-nav cf',               // adding custom nav class
+    					'theme_location' => 'minitop',                 // where it's located in the theme
+    					'before' => '',                                 // before the menu
+        			'after' => '',                                  // after the menu
+        			'link_before' => '',                            // before each link
+        			'link_after' => '',                             // after each link
+        			'depth' => 0,                                   // limit the depth of the nav
+    					'fallback_cb' => ''                             // fallback function (if there is one)
+						)); ?>
 
+						
+					</div>
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
 					<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="/wp-content/uploads/2014/10/logo.jpg"></a>
 
