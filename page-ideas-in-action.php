b6cb4eb6137adc6073a75e6ed3359d71df9be1fc
elvis <elvina@safetycat.org>
2014-11-04 18:19:43 +0000

    layout of ideas page

--- page-ideas-in-action.php
+++ page-ideas-in-action.php
@@ -4,7 +4,7 @@
 
 				<div id="inner-content" class="wrap cf">
 
-						<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+						<div id="main" class="m-all t-all d-all cf" role="main">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -12,11 +12,7 @@
 
 								<header class="article-header">
 
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-
-									<p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
+									<h1 class="page-title" itemprop="headline"><i class='fa fa-leaf'></i><?php the_title(); ?></h1>
 
 								</header> <?php // end article header ?>
 
@@ -28,16 +24,47 @@
 										echo do_shortcode('[searchandfilter id="603"]'); 
 
 
-										// if( isset($_POST) )
-										// 	echo "post";
-										
+										 // if( isset($_POST['_sf_submitted']) ){
+										 // 	echo "post";
+										 // echo do_shortcode('[searchandfilter id="603" show="results"]');
+										 // }
+										 	
 
-
-										// echo do_shortcode('[searchandfilter id="603" show="results"]');
 
 										// Custom Query Here.
-										
+										/* The 2nd Query (without global var) */
+										echo "<div id='ideas_grid' > ";
+										$args = array(
+											'post_type'  => 'idea',
+											'meta_query' => array(
+												array(
+													'key'     => 'featured_idea',
+													'value'   => '',
+													'compare' => '!=',
+												),
+											),
+										);
+										$query2 = new WP_Query( $args );
+
+										// The 2nd Loop
+										while ( $query2->have_posts() ) {
+											$query2->next_post();
+											echo '<li class="d-1of3 t-1of3 .m-all">';
+											echo get_the_post_thumbnail($query2->post->ID, 'idea-thumb' );
+											echo "<div class='ideabox'><i class='fa fa-leaf'></i>";
+											echo "<h3 class=''><a href='" . get_permalink($query2->post->ID ) . "'>" . get_the_title( $query2->post->ID ) . "</a></h3>";
+											echo '</div></li>';
+											$featured = get_post_meta($query2->post->ID, 'featured_idea', true);
+											//print_r($featured[0]);
+										}
+
+										// Restore original Post Data
+										wp_reset_postdata();
+
+
+
 										?>
+									</div>
 
 								</section> <?php // end article section ?>
 
@@ -67,7 +94,7 @@
 
 						</div>
 
-						<?php get_sidebar(); ?>
+						
 
 				</div>
 
