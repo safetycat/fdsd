e1003e37a86a0e136cdee2f492aa2b9865708605
elvis <elvina@safetycat.org>
2014-11-04 18:20:07 +0000

    remove date

--- search.php
+++ search.php
@@ -14,10 +14,7 @@
 								<header class="article-header">
 
 									<h3 class="search-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-
-                  <p class="byline vcard">
-                    <?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-                  </p>
+										
 
 								</header>
 
